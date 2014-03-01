308069dc3f248201105206ebca048f6eb77d1d25
joker1007 <kakyoin.hierophant@gmail.com>
2014-03-01 23:33:52 +0900

    Fix server definition

--- lib/octogate/server.rb
+++ lib/octogate/server.rb
@@ -5,15 +5,16 @@ class Octogate::Server < Sinatra::Base
     enable :logging
   end
 
-  get '/:token' do
+  post '/:token' do
     unless Octogate.config.token == params[:token]
       status 403
       body "Access forbidden"
       return
     end
 
+    p params
     # TODO: Fake Implementation
-    event = Octogate::Event::Push.parse(params.delete(:token))
+    event = Octogate::Event::Push.parse(Oj.load(params[:payload]))
     Octogate::Client.new(event).send_to_targets
 
     return
