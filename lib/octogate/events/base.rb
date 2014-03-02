2e4c627e285b3c09993f5ef50fbe8de5481f48a3
joker1007 <kakyoin.hierophant@gmail.com>
2014-03-02 20:50:25 +0900

    Add Event::PullRequest

--- lib/octogate/events/base.rb
+++ lib/octogate/events/base.rb
@@ -1,5 +1,11 @@
 module Octogate
   class Event::Base < Octogate::Model
+    class << self
+      def register_event(name, klass)
+        Octogate::Event.register_event(name, klass)
+      end
+    end
+
     def default_condition
       true
     end
---
+++ lib/octogate/events/pull_request.rb
@@ -0,0 +1,23 @@
+require "octogate/gh"
+
+module Octogate
+  class Event::PullRequest < Event::Base
+    register_event :pull_request, self
+
+    coerce_key :pull_request, GH::PullRequest
+    coerce_key :repository,   GH::Repository
+    coerce_key :sender,       GH::User
+
+    class << self
+      def parse(json)
+        payload = Oj.load(json).deep_symbolize_keys
+
+        new(payload)
+      end
+    end
+
+    def default_condition
+      !deleted
+    end
+  end
+end
